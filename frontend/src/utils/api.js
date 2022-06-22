5b27afea6c08d9d28cf8ae23a07f1ec38ad80340
kaplandavid992 <kaplandavid992@gmail.com>
2022-06-23 01:16:52 +0300

    corrt

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -75,9 +75,6 @@ const api = new Api({
   headers: {
     authorization: `Bearer ${token}`,
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
   },
 });
 
