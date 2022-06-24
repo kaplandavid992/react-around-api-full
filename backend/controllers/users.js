3f154cab43ca01205c2c1673e82e342110e71c14
kaplandavid992 <kaplandavid992@gmail.com>
2022-06-24 15:51:59 +0300

    current user

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -26,19 +26,32 @@ const getUsers = async (req, res, next) => {
   }
 };
 
-const getCurrentUser = async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    await User.findOne({ email })
-      .select('+password')
-      .orFail(() => {
-        const error = new Error('No user found');
-        error.statusCode = 404;
-      })
-      .then((user) => res.send(user));
-  } catch (error) {
-    defaultError(res);
-  }
+// const getCurrentUser = async (req, res) => {
+//   const { email, password } = req.body;
+//   try {
+//     await User.findOne({ email })
+//       .select('+password')
+//       .orFail(() => {
+//         const error = new Error('No user found');
+//         error.statusCode = 404;
+//       })
+//       .then((user) => res.send(user));
+//   } catch (error) {
+//     defaultError(res);
+//   }
+// };
+
+const getCurrentUser = (req, res, next) => {
+  User.findById(req.user._id)
+    .then((user) => {
+      if (!user) {
+        throw new ClassError(404, 'No user found with that id');
+      }
+      res.send({ user });
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
 
 const getUserById = async (req, res) => {
